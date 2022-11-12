a87f31cdadbf7c4a4b4e55a93311c2f831254005
Shadiya Egal <shadiya325@gmail.com>
2022-11-12 14:03:16 -0500

    commit tree 5a1827c87e61490aaa062822c5a4b7cd1dbeea79

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(names) {
+    const messages = []
+        for(let i = 0; i <names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    } 
+    return messages
+    }
+writeCards(messages)
+function countDown() {
+    let countDown = 10
+while(countDown >= 0) {
+    console.log(countDown--)
+}
+}
